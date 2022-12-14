0f6babc661897a6735cd8a5698a8ddb2e1a86b9c
Lesya Kosilova <lesyaaprok@gmail.com>
2022-12-15 02:59:19 +0300

    feat: make the game board clickable, add the ability to make a move for each player

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,12 +5,27 @@ import styles from "./../Board/Board.module.css";
 const Board = () => {
   const initialValues = Array(9).fill(null);
   const [squares, setSquares] = useState(initialValues);
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (squareNumber) => {
+    if (squares[squareNumber]) return;
+
+    const updatedSquares = [...squares];
+    updatedSquares[squareNumber] = isXNext ? "X" : "O";
+    setSquares(updatedSquares);
+    setIsXNext(!isXNext);
+  };
 
   return (
     <div className={styles.board}>
       {
         squares.map((square, i) => (
-          <Square value={square} key={i} />
+          <Square
+            value={square}
+            key={i}
+            onClick={() => handleClick(i)}
+            isXNext={isXNext}
+          />
         ))
       }
     </div>
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./Square.module.css";
 
-const Square = ({ value }) => {
-  return <div className={styles.square}>{value}</div>;
+const Square = ({ value, onClick }) => {
+  const color = value === "X" ? "text-grafit" : "text-pink";
+
+  return (
+    <div
+      className={`${styles.square} ${color}`}
+      onClick={() => onClick()}>
+      {value}
+    </div>
+  );
 };
 
 export default Square;

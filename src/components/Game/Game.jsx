0c0ddd7b4465e2db284a1a42668e94e7ba408625
Lesya Kosilova <lesyaaprok@gmail.com>
2022-12-15 19:06:19 +0300

    feat: add draw behavior

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -22,11 +22,14 @@ const Game = () => {
   const winnerLines = winner[1] || null;
   const color = isXNext ? "text-grafit" : "text-pink";
   const coloredName = <span className={color}>{isXNext ? " X" : " O"}</span>;
+  const isDraw = squares.every(square => square !== null);
 
   return (
     <div className={styles.game}>
       {winner ? (
         <h2 className={styles.info}>Player {winner[0]} won!</h2>
+      ) : isDraw ? (
+        <h2 className={styles.info}>Draw!</h2>
       ) : (
         <h2 className={styles.info}>Player {coloredName} is next!</h2>
       )}
